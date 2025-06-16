7ac246f5b0450f7d149f38d850527ad4f96f6e1a
Shallbony Biswas <shallbonybiswas@gmail.com>
2025-06-16 16:26:41 +0530

    Add files via upload

---
+++ problem1_list&tuples.py
@@ -0,0 +1,27 @@
+#name = [input ("Enter the name of 1st fruit:"), input ("Enter the name of 2nd fruit:"), input("Enter the name of 3rd fruit:"), input("Enter the name 4th fruit:"),input("Enter the name of 5th fruit:"),input("Enter the name of 6th fruit:"),input("Enter the name of 7th fruit:")]
+#print(name)
+
+
+
+
+
+
+
+
+
+fruits = []
+f1 = input("Enter fruit name:")
+fruits.append(f1)
+f2 = input("Enter fruit name:")
+fruits.append(f2)
+f3 = input("Enter fruit name:")
+fruits.append(f3)
+f4 = input("Enter fruit name:")
+fruits.append(f4)
+f5 = input("Enter fruit name:")
+fruits.append(f5)
+f6 = input("Enter fruit name:")
+fruits.append(f6)
+f7 = input("Enter fruit name:")
+fruits.append(f7)
+print(fruits)
